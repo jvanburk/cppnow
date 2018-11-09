97e48fcc87fbcc809ae34de890c519ffd29e8a39
Julien van Burk <j.vanburk@gmail.com>
2018-11-09 14:29:59 +0100

    initialize

---
+++ hello.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class Calculator
+{
+private:
+    /* data */
+public:
+    Calculator(/* args */){};
+    ~Calculator(){};
+    void PrimeGenerator(int x, int y);
+
+};
+
+int main (int argc, char const *argv[])
+{
+    cout << "Please enter two numbers" << endl;
+    Calculator calculator;
+    int x, y;
+    cin >>x>>y;
+    calculator.PrimeGenerator(x,y);
+    cin.ignore();
+    cin.get();
+
+    return 0; 
+}
+
+void Calculator::PrimeGenerator(int x, int y){
+    cout << x << " " << y << endl;
+}
